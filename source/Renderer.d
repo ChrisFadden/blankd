6641973e4927f68c68d082993f3981dca8cd780c
Nathan Braswell <miloignis@gmail.com>
2014-09-19 23:16:21 -0400

    Moved SDL GL setup to Window

---
+++ source/Renderer.d
@@ -0,0 +1,19 @@
+
+class Renderer {
+/*
+ *    Window window;
+ *    Camera camera;
+ *    Array!(GameObject) objects;
+ *
+ *    this(Window window, Camera camera) {
+ *        this.window = window;
+ *        this.camera = camera;
+ *    }
+ *
+ *    void draw() {
+ *        for (obj : objects)
+ *            obj.draw();
+ *        window.flip();
+ *    }
+ */
+}
